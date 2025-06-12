880d1241c48c79c100961abff2688710065b52f5
giselerotta <giselecrotta@gmail.com>
2025-06-11 21:14:55 -0300

    add base files from lab4

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "C:/Users/gisel/OneDrive/Documentos/Gisele/UFRGS/CIC/25_1/FCG/Laboratorio_4_Codigo_Fonte/bin/Debug/main.exe"
+  "C:/Users/gisel/OneDrive/Documentos/Gisele/UFRGS/CIC/25_1/FCG/Laboratorio_4_Codigo_Fonte/bin/Debug/main.exe.manifest"
+  "C:/Users/gisel/OneDrive/Documentos/Gisele/UFRGS/CIC/25_1/FCG/Laboratorio_4_Codigo_Fonte/bin/Debug/main.pdb"
+  "CMakeFiles/main.dir/src/glad.c.obj"
+  "CMakeFiles/main.dir/src/glad.c.obj.d"
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.obj"
+  "CMakeFiles/main.dir/src/textrendering.cpp.obj.d"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj"
+  "CMakeFiles/main.dir/src/tiny_obj_loader.cpp.obj.d"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
