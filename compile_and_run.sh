849cac4629358c3b190bc9c8d75e80455606910f
Isadora Vidor <isadora.vidor@icloud.com>
2025-07-09 20:09:54 -0300

    minor changes on compile_and_run script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,13 +1,8 @@
-cd 
-cd repo/Final-Project-FCG/
-
-sed -i 's/\.\.\/\.\.\///g' src/main.cpp
-
-g++ src/*.cpp src/glad.c -Iinclude -o Laboratorio4 -lglfw -ldl -lGL
+g++ src/*.cpp src/glad.c -Iinclude -o Final-Project-FCG -lglfw -ldl -lGL
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful!"
-    ./Laboratorio4
+    ./Final-Project-FCG
 else
     echo "Compilation failed."
 fi
